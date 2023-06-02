4a0d7efbbf9eac5b0e40cf1e0313e904a8301167
Mitali Shah <mitalishaleenshah@gmail.com>
2023-06-02 12:33:57 -0500

    mobile layout complete

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const summaryEl = document.getElementById("summary-list");
 const getData = async() => {
     const response = await fetch(`./data.json`)
     const data = await response.json()
-    data.forEach((item) => {
+    data.forEach((item, index) => {
+        // console.log(item,index)
         const summaryListItem = document.createElement("div");
         summaryListItem.innerHTML = `
-            <div class= 'summary-icon-category'>
                 <div class="summary-left">
                     <img src='${item.icon}'>
                     <h4>${item.category}</h4>
-                </div>    
-                <p>${item.score}<span> / 100</span></p>
-            </div>`
+                </div> 
+                <div class="summary-right">
+                    <p>${item.score}<span> / 100</span></p>
+                </div>`
                 
             summaryListItem.classList.add('summary-item')
             summaryEl.append(summaryListItem)
@@ -20,3 +21,4 @@ const getData = async() => {
 }
 
 getData()
+
